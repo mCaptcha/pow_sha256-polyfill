be7e76825c9e2d532f8724f79d55ed7784185cb2
Aravinth Manivannan <realaravinth@batsense.net>
2023-10-29 02:15:20 +0530

    feat: incremental proof of work computing fn

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { digest, score, generate_work } from "./index";
+import { digest, score, generate_work, stepped_generate_work } from "./index";
 import { DATA, DIFFICULTY, SALT } from "./test-data";
 
 ("use strict");
@@ -28,5 +28,35 @@ test("Digest works", async () => {
       console.log(`${d.pow.nonce}${error}`);
       throw error;
     }
+  }
+});
+
+test("Incremental proof generation works", async () => {
+  for (let i = 0; i < DATA.length; i++) {
+    const d = DATA[i];
+    let last_nonce = 0;
+    const step = 1000;
+    let fnExecuted = false;
+    const fn = (n: number): void => {
+      expect(last_nonce + step).toBe(n);
+      last_nonce = n;
+      fnExecuted = true;
+    };
+
+    try {
+      const proof = await stepped_generate_work(
+        SALT,
+        d.phrase,
+        DIFFICULTY,
+        1000,
+        fn
+      );
+      expect(proof.nonce).toBe(d.pow.nonce);
+      expect(proof.result).toBe(`${d.pow.result}`);
+    } catch (error) {
+      console.log(`${d.pow.nonce}${error}`);
+      throw error;
+    }
+    expect(fnExecuted).toBeTruthy();
   }
 });
